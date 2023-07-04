b8d3fb2af171b4ee0a7fc8d1a66769f2da33de48
Sarthak <sarthakapoor2152@gmail.com>
2023-07-05 00:51:44 +0530

    first commit of Random color generator

---
+++ script.js
@@ -0,0 +1,22 @@
+function randomRGB() {
+  let num = Math.floor(Math.random() * 1000);
+  num = num % 256;
+  return num;
+}
+
+function setColor() {
+  let red = randomRGB();
+  let blue = randomRGB();
+  let green = randomRGB();
+
+  let h3 = document.querySelector("h3");
+  h3.innerText = `RGB ( ${red}, ${blue} ,${green})`;
+
+  document.body.style.backgroundColor = `rgb(${red},${blue},${green})`;
+
+  let btn = document.querySelector(".generate");
+  btn.style.color = `rgb(${red},${blue},${green})`;
+}
+
+let btn = document.querySelector(".generate");
+btn.addEventListener("click", setColor);
