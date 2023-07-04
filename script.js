0ad79aa26b59ce171062b6f6b295e8d2d153e4db
Sarthak <sarthakapoor2152@gmail.com>
2023-07-05 00:55:17 +0530

    done a few corrections in javascript file

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ function setColor() {
   let green = randomRGB();
 
   let h3 = document.querySelector("h3");
-  h3.innerText = `RGB ( ${red}, ${blue} ,${green})`;
+  h3.innerText = `RGB ( ${red}, ${green} ,${blue})`;
 
-  document.body.style.backgroundColor = `rgb(${red},${blue},${green})`;
+  document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
 
   let btn = document.querySelector(".generate");
-  btn.style.color = `rgb(${red},${blue},${green})`;
+  btn.style.color = `rgb(${red},${green},${blue})`;
 }
 
 let btn = document.querySelector(".generate");
